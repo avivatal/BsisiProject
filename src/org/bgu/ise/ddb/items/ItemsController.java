ba38e5fe3aa51df271aa36090f8fb47d7852bce7
avivatal <34033238+avivatal@users.noreply.github.com>
2019-01-01 22:02:39 +0200

    Initial commit

---
+++ src/org/bgu/ise/ddb/items/ItemsController.java
@@ -0,0 +1,184 @@
+/**
+ * 
+ */
+package org.bgu.ise.ddb.items;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.bgu.ise.ddb.MediaItems;
+import org.bgu.ise.ddb.ParentController;
+import org.bgu.ise.ddb.User;
+import org.bson.Document;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+
+
+
+/**
+ * @author Alex
+ *
+ */
+@RestController
+@RequestMapping(value = "/items")
+public class ItemsController extends ParentController {
+	
+	public static final String localhost = "localhost";
+	public static final int port = 27017;
+    Connection conn = null;
+    private final static String driver = "oracle.jdbc.driver.OracleDriver";
+    private String connectionUrl = "jdbc:oracle:thin:@ora1.ise.bgu.ac.il:1521/ORACLE";
+    private String username = "talshemt";
+    private String password = "talShem3";
+    
+    private void connect() {
+        try {
+        	Class.forName(driver);
+            conn = DriverManager.getConnection(connectionUrl, username, password);
+            conn.setAutoCommit(false);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch(ClassNotFoundException e) {
+        	e.printStackTrace();
+        }
+    }
+	/**
+	 * The function copy all the items(title and production year) from the Oracle table MediaItems to the System storage.
+	 * The Oracle table and data should be used from the previous assignment
+	 */
+	@RequestMapping(value = "fill_media_items", method={RequestMethod.GET})
+	public void fillMediaItems(HttpServletResponse response){
+		System.out.println("was here");
+		HttpStatus status = HttpStatus.OK;
+		MongoClient client = null;
+		ResultSet rs = null;
+		try {
+			
+	        if (conn == null || conn.isClosed()) {
+	            connect();
+	        }
+			client = new MongoClient(localhost,port);
+			MongoCollection table = client.getDatabase("local").getCollection("MediaItems");
+			
+	        String query = "SELECT * FROM MEDIAITEMS";
+            PreparedStatement ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();            
+            while (rs.next()) {            	
+				Document movie = new Document();
+				movie.append("Title", rs.getString("TITLE"));
+				movie.append("Prod_Year", rs.getString("PROD_YEAR"));				
+				table.insertOne(movie);
+            }
+            rs.close();
+            conn.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			
+		} finally {
+            client.close();
+		}
+		
+		response.setStatus(status.value());
+	}
+	
+	
+
+	/**
+	 * The function copy all the items from the remote file,
+	 * the remote file have the same structure as the films file from the previous assignment.
+	 * You can assume that the address protocol is http
+	 * @throws IOException 
+	 */
+	@RequestMapping(value = "fill_media_items_from_url", method={RequestMethod.GET})
+	public void fillMediaItemsFromUrl(@RequestParam("url")    String urladdress,
+			HttpServletResponse response) throws IOException{
+		System.out.println(urladdress);
+		HttpStatus status = HttpStatus.OK;
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+		MongoClient client = null;
+        
+		try {
+			client = new MongoClient(localhost,port);
+			MongoCollection table = client.getDatabase("local").getCollection("MediaItems");
+			br = new BufferedReader(new InputStreamReader(new URL(urladdress).openStream()));
+            //reads each line and inserts new record
+            while ((line = br.readLine()) != null) {
+                String[] movie = line.split(cvsSplitBy);
+                movie[0] = movie[0].replace("'", "''");
+                
+                Document movieDoc = new Document();
+                movieDoc.append("Title", movie[0]);
+                movieDoc.append("Prod_Year", movie[1]);
+                table.insertOne(movieDoc);
+            } 
+            status = HttpStatus.OK;
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		
+		response.setStatus(status.value());
+	}
+	
+	
+	/**
+	 * The function retrieves from the system storage N items,
+	 * order is not important( any N items) 
+	 * @param topN - how many items to retrieve
+	 * @return
+	 */
+	@RequestMapping(value = "get_topn_items",headers="Accept=*/*", method={RequestMethod.GET},produces="application/json")
+	@ResponseBody
+	@org.codehaus.jackson.map.annotate.JsonView(MediaItems.class)
+	public  MediaItems[] getTopNItems(@RequestParam("topn")    int topN){
+		//:TODO your implementation
+		
+		ArrayList<MediaItems> movies = new ArrayList();
+		MongoClient client = null;
+		int counter = 0;
+		try {
+			
+			client = new MongoClient(localhost, port);
+			MongoCollection table = client.getDatabase("local").getCollection("MediaItems");			
+			MongoCursor allmovies = table.find().iterator();
+			while(allmovies.hasNext() && counter<topN) {
+				Document user = (Document)allmovies.next();
+				movies.add(new MediaItems(user.get("Title").toString(),Integer.parseInt(user.get("Prod_Year").toString())));
+				counter++;
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		return movies.toArray(new MediaItems[movies.size()]);
+	}
+		
+
+}
---
+++ src/org/bgu/ise/ddb/registration/RegistarationController.java
@@ -0,0 +1,216 @@
+/**
+ * 
+ */
+package org.bgu.ise.ddb.registration;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.model.Filters;
+
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.bgu.ise.ddb.ParentController;
+import org.bgu.ise.ddb.User;
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
+import org.bson.Document;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Alex
+ *
+ */
+@RestController
+@RequestMapping(value = "/registration")
+public class RegistarationController extends ParentController{
+
+	public static final String localhost = "localhost";
+	public static final int port = 27017;
+
+
+	/**
+	 * The function checks if the username exist,
+	 * in case of positive answer HttpStatus in HttpServletResponse should be set to HttpStatus.CONFLICT,
+	 * else insert the user to the system  and set to HttpStatus in HttpServletResponse HttpStatus.OK
+	 * @param username
+	 * @param password
+	 * @param firstName
+	 * @param lastName
+	 * @param response
+	 */
+	@RequestMapping(value = "register_new_customer", method={RequestMethod.POST})
+	public void registerNewUser(@RequestParam("username") String username,
+			@RequestParam("password")    String password,
+			@RequestParam("firstName")   String firstName,
+			@RequestParam("lastName")  String lastName,
+			HttpServletResponse response){
+		System.out.println(username+" "+password+" "+lastName+" "+firstName);
+		MongoClient client = null;
+		try {
+			HttpStatus status = HttpStatus.BAD_REQUEST;
+			if(isExistUser(username)) {
+				System.out.println("false");
+				status = HttpStatus.CONFLICT;
+			} else {
+				System.out.println("true");
+				client = new MongoClient(localhost,port);
+				MongoCollection table = client.getDatabase("local").getCollection("Users");
+
+				Document user = new Document();
+				user.append("Username", username);
+				user.append("Password", password);
+				user.append("Firstname", firstName);
+				user.append("Lastname", lastName);
+				user.append("Date", Instant.now());
+				table.insertOne(user);
+				status = HttpStatus.OK;
+			}
+			response.setStatus(status.value());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			client.close();
+		}
+	}
+
+	/**
+	 * The function returns true if the received username exist in the system otherwise false
+	 * @param username
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "is_exist_user", method={RequestMethod.GET})
+	public boolean isExistUser(@RequestParam("username") String username) throws IOException{
+		System.out.println(username);
+		boolean result = false;
+		MongoClient client = null;
+
+		try {
+			client = new MongoClient(localhost, port);
+			MongoCollection table = client.getDatabase("local").getCollection("Users");
+			Document res = new Document();
+			res.append("Username", username);
+			MongoCursor iterator = table.find(res).iterator();
+
+			while(iterator.hasNext()) {
+				result = true;
+				break;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		return result;
+
+	}
+
+	/**
+	 * The function returns true if the received username and password match a system storage entry, otherwise false
+	 * @param username
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "validate_user", method={RequestMethod.POST})
+	public boolean validateUser(@RequestParam("username") String username,
+			@RequestParam("password")    String password) throws IOException{
+		System.out.println(username+" "+password);
+		boolean result = false;
+		MongoClient client = null;
+
+		try {
+			client = new MongoClient(localhost, port);
+			MongoCollection table = client.getDatabase("local").getCollection("Users");
+			Document res = new Document();
+			res.append("Username", username);
+			res.append("Password", password);
+			MongoCursor iterator = table.find(res).iterator();
+
+			while(iterator.hasNext()) {
+				result = true;
+				break;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		return result;
+	}
+
+	/**
+	 * The function retrieves number of the registered users in the past n days
+	 * @param days
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping(value = "get_number_of_registred_users", method={RequestMethod.GET})
+	public int getNumberOfRegistredUsers(@RequestParam("days") int days) throws IOException{
+		System.out.println(days+"");
+		int result = 0;
+		
+		MongoClient client = null;
+		try {
+			
+			client = new MongoClient(localhost, port);
+			MongoCollection table = client.getDatabase("local").getCollection("Users");			
+			MongoCursor allUsersInLastNdays = table.find(Filters.gt("Date", Instant.now().minus(days,ChronoUnit.DAYS))).iterator();
+			while(allUsersInLastNdays.hasNext()) {
+				result++;
+				allUsersInLastNdays.next();
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+
+		return result;
+
+	}
+
+	/**
+	 * The function retrieves all the users
+	 * @return
+	 */
+	@RequestMapping(value = "get_all_users",headers="Accept=*/*", method={RequestMethod.GET},produces="application/json")
+	@ResponseBody
+	@org.codehaus.jackson.map.annotate.JsonView(User.class)
+	public  User[] getAllUsers(){
+		
+		ArrayList<User> users = new ArrayList();
+		MongoClient client = null;
+
+		try {
+			
+			client = new MongoClient(localhost, port);
+			MongoCollection table = client.getDatabase("local").getCollection("Users");			
+			MongoCursor allUsers = table.find().iterator();
+			while(allUsers.hasNext()) {
+				Document user = (Document)allUsers.next();
+				users.add(new User(user.get("Username").toString(),user.get("Password").toString(),user.get("Firstname").toString(),user.get("Lastname").toString()));
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			client.close();
+		}
+		return users.toArray(new User[users.size()]);
+	}
+
+}
